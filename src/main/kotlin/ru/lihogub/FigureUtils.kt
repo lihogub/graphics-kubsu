11b71989de0a238f7125dbde007664cacd528d7f
Lihogub Oleg <oligli@yandex.ru>
2021-10-24 17:24:35 +0300

    added simple box drawer

---
+++ src/main/kotlin/ru/lihogub/FigureUtils.kt
@@ -0,0 +1,60 @@
+package ru.lihogub
+
+import org.lwjgl.opengl.GL46
+
+object FigureUtils {
+    fun drawBox(
+        red: Double, green: Double, blue: Double,
+        x: Double, y: Double, z: Double,
+        dX: Double, dY: Double, dZ: Double
+    ) {
+        GL46.glBegin(GL46.GL_QUADS)
+        GL46.glColor3d(red, green, blue)
+        GL46.glVertex3d(x, y, z)
+        GL46.glVertex3d(x + dX, y, z)
+        GL46.glVertex3d(x + dX, y +dY, z)
+        GL46.glVertex3d(x, y + dY, z)
+        GL46.glEnd()
+
+        GL46.glBegin(GL46.GL_QUADS)
+        GL46.glColor3d(red, green, blue)
+        GL46.glVertex3d(x, y, z + dZ)
+        GL46.glVertex3d(x + dX, y, z + dZ)
+        GL46.glVertex3d(x + dX, y +dY, z + dZ)
+        GL46.glVertex3d(x, y + dY, z + dZ)
+        GL46.glEnd()
+
+
+        GL46.glBegin(GL46.GL_QUADS)
+        GL46.glColor3d(red, green, blue)
+        GL46.glVertex3d(x, y, z)
+        GL46.glVertex3d(x, y + dY, z)
+        GL46.glVertex3d(x, y + dY, z + dZ)
+        GL46.glVertex3d(x, y, z + dZ)
+        GL46.glEnd()
+
+        GL46.glBegin(GL46.GL_QUADS)
+        GL46.glColor3d(red, green, blue)
+        GL46.glVertex3d(x + dX, y, z)
+        GL46.glVertex3d(x + dX, y + dY, z)
+        GL46.glVertex3d(x + dX, y + dY, z + dZ)
+        GL46.glVertex3d(x + dX, y, z + dZ)
+        GL46.glEnd()
+
+        GL46.glBegin(GL46.GL_QUADS)
+        GL46.glColor3d(red, green, blue)
+        GL46.glVertex3d(x, y, z)
+        GL46.glVertex3d(x + dX, y, z)
+        GL46.glVertex3d(x + dX, y, z + dZ)
+        GL46.glVertex3d(x, y, z + dZ)
+        GL46.glEnd()
+
+        GL46.glBegin(GL46.GL_QUADS)
+        GL46.glColor3d(red, green, blue)
+        GL46.glVertex3d(x, y + dY, z)
+        GL46.glVertex3d(x + dX, y + dY, z)
+        GL46.glVertex3d(x + dX, y + dY, z + dZ)
+        GL46.glVertex3d(x, y + dY, z + dZ)
+        GL46.glEnd()
+    }
+}
